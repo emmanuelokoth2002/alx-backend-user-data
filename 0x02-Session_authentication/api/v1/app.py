50600a0fc750412fa9e2d946fc0b77f40cefab30
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-11-09 12:25:53 -0800

    Updates

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,14 +7,14 @@ Main Flask app for handling routes and errors.
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 auth = None
-auth_type = os.getenv("AUTH_TYPE")
+auth_type = getenv("AUTH_TYPE")
 
 if auth_type == "auth":
     from api.v1.auth.auth import Auth
@@ -32,8 +32,8 @@ def before_request():
     if auth is None:
         pass
     else:
-        excluded_list = ['/api/v1/status/',
-                         '/api/v1/unauthorized/', '/api/v1/forbidden/']
+        excluded_list = ['/api/v1/status/', '/api/v1/unauthorized/',
+                         '/api/v1/forbidden/']
         if auth.require_auth(request.path, excluded_list):
             if auth.authorization_header(request) is None:
                 abort(401, description='Unauthorized')
@@ -63,7 +63,6 @@ def forbidden(error) -> str:
     Handler for 403 - Forbidden.
     """
     return jsonify({"error": "Forbidden"}), 403
-
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
