a24c409af13e582656c77d51337a55c01b74da0c
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-11-09 05:57:22 -0800

    Updates

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+'''
+Module for User Authentication
+'''
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    ''' Class handling authentication '''
+
+    def require_auth(self, requested_path: str,
+                     exclude_list: List[str]) -> bool:
+        ''' Check if authentication is required '''
+        if requested_path is None:
+            return True
+        if exclude_list is None or exclude_list == []:
+            return True
+        if requested_path in exclude_list:
+            return False
+
+        for excluded_path in exclude_list:
+            if excluded_path.startswith(requested_path):
+                return False
+            elif requested_path.startswith(excluded_path):
+                return False
+            elif excluded_path[-1] == '*':
+                if requested_path.startswith(excluded_path[:-1]):
+                    return False
+        return True
+
+    def authorization_header(self, req=None) -> str:
+        ''' Get the authorization header '''
+        if req is None:
+            return None
+        auth_header = req.headers.get('Authorization')
+        if auth_header is None:
+            return None
+        return auth_header
+
+    def current_user(self, req=None) -> TypeVar('User'):
+        ''' Fetch the current user '''
+        return None
