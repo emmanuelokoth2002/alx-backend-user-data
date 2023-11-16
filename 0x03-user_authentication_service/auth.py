1fb565296d2f7e62728cb9ba73b0a9fb2702f012
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-11-16 12:27:01 -0800

    Updates

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,8 @@ Auth module
 """
 
 import bcrypt
+from user import User
+from db import DB
 
 
 def _hash_password(password: str) -> bytes:
@@ -20,3 +22,14 @@ def _hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
     return hashed_password
+
+
+def register_user(self, email: str, password: str) -> User:
+    """Register a new user."""
+    existing_user = self._db.find_user_by_email(email)
+    if existing_user:
+        raise ValueError(f"User {email} already exists")
+    hashed_password = self._hash_password(password)
+    new_user = self._db.add_user(email, hashed_password)
+
+    return new_user
