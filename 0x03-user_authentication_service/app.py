e1a10d6d6e2b67ae63bf7057fdbd96c7846bf487
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-11-16 18:11:44 -0800

    Updates

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,8 +4,10 @@ Flask App for User Authentication Service
 """
 
 from flask import Flask, jsonify
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
@@ -17,6 +19,30 @@ def welcome():
         jsonify: JSON response {"message": "Bienvenue"}
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    """
+    Endpoint to register a user.
+
+    Expects form data fields: "email" and "password".
+    Responds with JSON payload based on registration success or failure.
+
+    Returns:
+        jsonify: JSON response based on registration status.
+    """
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        new_user = AUTH.register_user(email, password)
+
+        return jsonify({"email": new_user.email, "message": "user
+                        created"}), 200
+
+    except ValueError as e:
+        return jsonify({"message": str(e)}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
